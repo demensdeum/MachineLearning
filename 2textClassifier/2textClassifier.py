26ffdac12fefe3fd97bf49096ffc939cbc2e8c54
demensdeum <demensdeum@gmail.com>
2017-12-09 12:32:48 +0400

    Input commands, text length handling for input text classifier

--- 2textClassifier/2textClassifier.py
+++ 2textClassifier/2textClassifier.py
@@ -9,8 +9,9 @@ def inputDatasetFunction():
     global inputString 
     
     inputDatasetDictionary = {}
-    
+	
     ClassifiedStringsDatasetTensorFlowData.appendCharactersListToFeaturesDictionary(inputString, inputDatasetDictionary, trainDatasetData.datasetLineMaxLength())
+    
     
     return inputDatasetDictionary
     
@@ -27,15 +28,31 @@ def main():
     trainDatasetData = trainDataset.trainDatasetData()
 
     classifier = tensorflow.estimator.DNNClassifier(feature_columns = trainDatasetData.feature_columns(), model_dir=".", hidden_units = [512, 256, 128], n_classes = trainDatasetData.n_classes())
-    classifier.train(input_fn = trainDataset.trainDatasetFunction, steps = 200000)
+    
+    state = input("train/classify? ")
+    
+    if state == "train":
+        print("Endless train mode, CTRL+C for exit")
+        classifier.train(input_fn = trainDataset.trainDatasetFunction)
+	
+    elif state == "classify":
+        print("Text classify mode, empty text input to exit")
+	
+    else:
+        print("Write train or classify... Exit")
+        exit(1)
     
     while True:
     
         inputString = input("Text to classify: ")
     
         if len(inputString) < 1:
-            print("Exit...")
+            print("Empty text to classify. Exit...")
             exit(0)
+	    
+        elif (len(inputString) > trainDatasetData.datasetLineMaxLength()):
+            print("Input text is too long")
+            continue
     
         generator = classifier.predict(input_fn = inputDatasetFunction)
     
